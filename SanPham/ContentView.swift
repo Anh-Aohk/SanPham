16ec449a0f6b9351695fe7c10b19832c11cade87
Pham Anh Khoa <miloemaster@gmail.com>
2026-05-24 09:59:36 +0700

    Initial Commit

---
+++ SanPham/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SanPham
+//
+//  Created by Phạm Anh Khoa on 24/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SanPham/SanPhamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanPhamApp.swift
+//  SanPham
+//
+//  Created by Phạm Anh Khoa on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SanPhamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
